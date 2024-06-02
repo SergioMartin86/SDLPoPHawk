d07d85b2b2ada56a04b09485980f0562990f8962
Sergio Martin <martiser@ethz.ch>
2024-06-02 12:54:09 +0200

    Removing more stuff

--- ExternalProjects/libchd-rs-capi/build_release.bat
+++
@@ -1,3 +0,0 @@
-@cargo b --release
-@copy target\release\chd_capi.dll ..\..\Assets\dll
-@copy target\release\chd_capi.dll ..\..\output\dll
--- ExternalToolProjects/.run_with_tool.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-../../output/EmuHawkMono.sh --mono-no-redirect --open-ext-tool-dll=$(printf *.csproj | head -c-7) "$@"
